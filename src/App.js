92e78dd8a113118cc286ccab241490c1d54fb9cb
Sergio Meneses <chicoboliviano16@gmail.com>
2019-07-11 20:29:23 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/navbar/Navbar'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar'
+
+
+const Navbar = () => 
+
+    <AppBar title="PixaBar Image Finder"/>
+
+
+export default Navbar
